e9fa0216a6fa1ac4e70f7231cc8bc6f4c67bb1c8
Jay Patel <jcp313@gmail.com>
2016-11-11 17:55:17 -0500

    inital checkout changes

--- TheGreenRoomLibrary/TheGreenRoomLibrary/Checkout.cs
+++ TheGreenRoomLibrary/TheGreenRoomLibrary/Checkout.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace TheGreenRoomLibrary
 {
-    class Checkout
+    public class Checkout:BookListClass
     {
+        public Checkout(author):base.
+        {
+            this.
+        }
+
+        public List<Book> CheckoutMethod(readFile.BookArray)
+        {
+            
+            if (readFile.BookArray == true)
+            {
+                readFile.BookArray.Add(new Book(rc[0], rc[1], check, DateTime.Parse(rc[3])));
+            }
+            else if (check == false)
+            {
+                BookArray.Add(new Book(rc[0], rc[1], check));
+            }
+        }
+
+
     }
 }
