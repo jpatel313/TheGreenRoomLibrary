04fe3696b8a5ae2f2e7b5eba308f7b17e4a104ad
Mary Olive <mary.olive@me.com>
2016-11-16 11:58:52 -0500

    add unit test

---
+++ TheGreenRoomLibrary/TheGreenRoomLibrary/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Collections.Generic;
+
+//namespace UnitTestsGRL
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        public object BookListClass { get; private set; }
+
+//        [TestMethod]
+//        //public void TestCheckoutBookMAIN()
+//        //{
+//        //    List<Book> testList = new List<Book> { new Book(2, "Fake Alice") };
+//        //    Library library = new Library(listOfBooks);
+//        //    Assert.IsTrue(library.isBookAvailable(2));
+//        //    library.checkoutBook(2);
+//        //    Assert.IsFalse(library.isBookAvailable(2));
+//        //    Assert.AreEqual()
+//        //}
+//        [TestMethod]
+//        public void TestCheckoutBook()
+//        {
+            
+//            Assert.AreEqual()
+//        }
+//    }
+//}
---
+++ TheGreenRoomLibrary/UnitTestGRL/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnitTestGRL
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreateFile()
+        {
+            
+            string filelocation = "../../test.txt";
+            File.Create(filelocation);
+            
+            Assert.AreEqual(File.Exists(filelocation), true);
+        }
+        [TestMethod]
+        public void TestWriteToFile()
+        {
+            string filelocation = "../../test.txt";
+            StreamWriter writer = new StreamWriter(filelocation);
+            List<String> testList = new List<string> { "blah", "blah blah", "boo" };
+            foreach (string word in testList)
+            {
+                writer.Write(word);
+            }
+            writer.Close();
+
+            string[] readBlahs = File.ReadAllLines(filelocation);
+            
+            writer.Dispose();
+            Assert.AreEqual(readBlahs.Length, 3);
+        }
+    }
+}
