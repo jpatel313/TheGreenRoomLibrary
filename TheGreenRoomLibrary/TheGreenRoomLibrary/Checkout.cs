a4cc28acd2b258a77f23503c66811b6d4385bd54
Jay Patel <jcp313@gmail.com>
2016-11-16 11:29:54 -0500

    checkOut comments

--- TheGreenRoomLibrary/TheGreenRoomLibrary/Checkout.cs
+++ TheGreenRoomLibrary/TheGreenRoomLibrary/Checkout.cs
@@ -8,6 +8,7 @@ namespace TheGreenRoomLibrary
 {
     public class Checkout
     {
+        //Ref parameter to pass changes to Book List. Leaves text file unchanged.
         public static void CheckInMethod(ref Book books)
         {
             if (books.Status == true)
@@ -17,16 +18,21 @@ namespace TheGreenRoomLibrary
 
                 if (yes == "y" && yes != null)
                 {
-                    
+                    //DateTime used to calculate due date.
                     DateTime time = DateTime.Now;
+
                     if (books.DueDate.CompareTo(time) > 0)
+
                     {
                         TimeSpan diff = books.DueDate.Subtract(time);
                         int daysOver = diff.Days;
                         double lateFee = daysOver * 0.1;
+
                         Console.WriteLine("This book is " + daysOver + " days overdue. You owe $" + lateFee + ".");
                     }
+
                     books.Status = false;
+
                     Console.WriteLine("Thank you for returning this book!");
                 }
 
@@ -35,34 +41,37 @@ namespace TheGreenRoomLibrary
             }
         }
 
+        //Changes bool in Book List to reflect checkout. False = not checked out.
         public static void CheckoutMethod(ref Book books)
         {
-
+           
             if (books.Status == false)
             {
                 books.Status = true;
 
                 books.DueDate = DateTime.Now.AddDays(14);
 
-                Console.WriteLine($"You have successfuly checked this book out. Due date: {books.DueDate}");
+                Console.WriteLine($"You have successfuly checked this book out. Due date: {books.DueDate} ");
             }
 
             else
             {
-                Console.WriteLine("That book is not available.  Please choose another book.");
+                Console.WriteLine("That book is not available.  Please choose another book...");
             }
         }
 
         public static Book Donate()
         {
-            String author;String title;
-            Console.WriteLine("What is the name of the book you would like to add?");
-            title = Console.ReadLine();
-            Console.WriteLine("Who wrote this book?");
-            author = Console.ReadLine();
-            Book B = new Book(title, " "+ author, false);
+
+            Console.WriteLine("What is the name of the book you would like to add? ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Who wrote this book? ");
+            string author = Console.ReadLine();
+
+            Book B = new Book(title, " " + author, false);
+
             return B;
         }
-
     }
 }
